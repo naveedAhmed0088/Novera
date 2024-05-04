ea6d1ad3f015886f9fafe9cec8cda849d952451b
Sahil-ali009 <94895263+Sahil-ali009@users.noreply.github.com>
2024-05-04 15:00:21 +0500

    latest commit

--- Novera/obj/Debug/net8.0-ios/iossimulator-x64/Novera.AssemblyInfo.cs
+++ Novera/obj/Debug/net8.0-ios/iossimulator-x64/Novera.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Novera")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+6e2e719bca39509845c8a482ce91b4460494e220")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+4438646ab92f212d50710fda099cf52ced2194df")]
 [assembly: System.Reflection.AssemblyProductAttribute("Novera")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Novera")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
