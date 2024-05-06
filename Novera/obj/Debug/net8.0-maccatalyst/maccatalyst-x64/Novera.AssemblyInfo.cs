92406713da77c7ab375ce0ed14f5a7c91f362fa3
Sahil-ali009 <94895263+Sahil-ali009@users.noreply.github.com>
2024-05-06 12:31:33 +0500

    git files

--- Novera/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/Novera.AssemblyInfo.cs
+++ Novera/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/Novera.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Novera")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+5ab35646a5e8674c19dbb21bdb561c5a9212b64e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+8c9ffe3cdd31e6dcd4a4c24897d415480fe90a94")]
 [assembly: System.Reflection.AssemblyProductAttribute("Novera")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Novera")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
