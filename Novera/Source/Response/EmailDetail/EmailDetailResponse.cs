525cace40016ac90dc08a18f6b0d7398b2a8466a
naveedAhmed007 <naveed.ahmed007@ymail.com>
2024-05-08 14:52:02 +0500

    email detail response

--- Novera/Source/Response/EmailDetail/EmailDetailResponse.cs
+++ Novera/Source/Response/EmailDetail/EmailDetailResponse.cs
@@ -1,27 +1,37 @@
-﻿public class Data
-{
-    public EmailDetails emailDetails { get; set; }
-    public List<object> documents { get; set; }
-}
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class EmailDetails
+namespace Novera.Source.Response.EmailDetail
 {
-    public int mailId { get; set; }
-    public string sender { get; set; }
-    public string senderName { get; set; }
-    public string sendTo { get; set; }
-    public string subject { get; set; }
-    public DateTime received { get; set; }
-    public string body_Text { get; set; }
-    public bool unread { get; set; }
-    public string important { get; set; }
-    public string uid { get; set; }
-}
+    public class Data
+    {
+        public EmailDetails emailDetails { get; set; }
+        public List<object> documents { get; set; }
+    }
 
-public class EmailDetailResponse
-{
-    public bool success { get; set; }
-    public string message { get; set; }
-    public Data data { get; set; }
-}
+    public class EmailDetails
+    {
+        public int mailId { get; set; }
+        public string sender { get; set; }
+        public string senderName { get; set; }
+        public string sendTo { get; set; }
+        public string subject { get; set; }
+        public DateTime received { get; set; }
+        public string body_Text { get; set; }
+        public bool unread { get; set; }
+        public string important { get; set; }
+        public string uid { get; set; }
+    }
 
+    public class EmailDetailResponse
+    {
+        public bool success { get; set; }
+        public string message { get; set; }
+        public Data data { get; set; }
+    }
+
+
+}
