f218aead9ff33d1747dbba4cfe5e0f7b291c14d8
naveedAhmed007 <naveed.ahmed007@ymail.com>
2024-05-08 18:12:31 +0500

    remove draft,srent

--- Novera/Source/Pages/Crm/Email/Drafts/DraftPage.xaml.cs
+++ Novera/Source/Pages/Crm/Email/Drafts/DraftPage.xaml.cs
@@ -13,7 +13,7 @@ public partial class DraftPage : ContentView
     private readonly HttpClient _client = new HttpClient();
     EmailApiService apiService;
     private readonly DraftViewModel _viewModel;
-    HashSet<string> addedMailIds;
+   
 
 #pragma warning disable CS8602
 #pragma warning disable CS8600
@@ -26,7 +26,6 @@ public partial class DraftPage : ContentView
         _viewModel = new DraftViewModel();
         BindingContext = _viewModel;
 
-        addedMailIds = new HashSet<string>();
     }
     private void AddClicked(object sender, EventArgs e)
     {
@@ -155,103 +154,7 @@ public partial class DraftPage : ContentView
     }
 
 
-    private void MultiSelectionTap(object sender, TappedEventArgs e)
-    {
-        var frame = (Frame)sender;
-        var item = (Datum)frame.BindingContext;
-
-        String mailId = item.mailId.ToString();
-        if (addedMailIds.Contains(mailId))
-        {
-            addedMailIds.Remove(mailId);
-
-            var label = (Label)frame.Content;
-            label.Text = item.sender.Substring(0, 1);
-        }
-        else
-        {
-            addedMailIds.Add(mailId);
-            var label = (Label)frame.Content;
-            label.Text = "\u221A";
-        }
-        if (addedMailIds.Count > 0)
-        {
-            Search.IsVisible = false;
-            emailsCount.IsVisible = true;
-            emailsCount.Text = addedMailIds.Count.ToString();
-            image.Source = "document_delete_folder";
-        }
-        else
-        {
-            Search.IsVisible = true;
-            emailsCount.IsVisible = false;
-
-            image.Source = "email_search";
-        }
-
-    }
-
-    private async void image_search_clicked(object sender, TappedEventArgs e)
-    {
-        if (addedMailIds.Count > 0)
-        {
-            bool confirmDelete = await App.Current.MainPage.DisplayAlert("Confirm Deletion", "Are you sure you want to delete these emails?", "Yes", "No");
-
-            if (confirmDelete)
-            {
-                try
-                {
-                    loader.IsRunning = true;
-                    loader.IsVisible = true;
-
-                    // Retrieve OAuth token from SecureStorage
-                    string oauthToken = await SecureStorage.Default.GetAsync("oauth_token");
-                    if (string.IsNullOrEmpty(oauthToken))
-                    {
-                        Console.WriteLine("OAuth token not found.");
-                        return;
-                    }
-
-                    string csvMailIds = string.Join("%2C", addedMailIds);
-                    string url = $"{ApiUrls.BaseUrl}Emails/{csvMailIds}";
-                    
-
-                    var response = await apiService.deleteEmail(url, oauthToken);
-                    if (response is InboxPageMarkResponse successResponse)
-                    {
-
-
-                        addedMailIds.Clear();
-                        Search.IsVisible = true;
-                        emailsCount.IsVisible = false;
-                        loader.IsRunning = false;
-                        loader.IsVisible = false;
-                        image.Source = "email_search";
-
-                        await _viewModel.RefreshData();
-                    }
-
-
-
-                }
-                catch (Exception ex)
-                {
-                    // Handle exception
-                    Console.WriteLine($"Exception: {ex.Message}");
-                    await App.Current.MainPage.DisplayAlert("Error", ex.Message, "OK");
-                }
-                finally
-                {
-                    loader.IsRunning = false;
-                    loader.IsVisible = false;
-                }
-            }
-        }
-        else
-        {
-            // No emails selected, handle accordingly
-        }
-    }
+    
 
     private async void EmailItemTapped(object sender, TappedEventArgs e)
     {
