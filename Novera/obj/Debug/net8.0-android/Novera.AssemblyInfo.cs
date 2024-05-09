56fc79040cdb642fb9a67866c90b4d4fade5bde7
naveedAhmed007 <naveed.ahmed007@ymail.com>
2024-05-09 15:32:19 +0500

    gitfiles

--- Novera/obj/Debug/net8.0-android/Novera.AssemblyInfo.cs
+++ Novera/obj/Debug/net8.0-android/Novera.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Novera")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+2dc0a5fd4fe6086d4a2b8816fc41c05d249c5a0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+6a7c76bff39d3a088d07b4e2a25bb9203599b85d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Novera")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Novera")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
