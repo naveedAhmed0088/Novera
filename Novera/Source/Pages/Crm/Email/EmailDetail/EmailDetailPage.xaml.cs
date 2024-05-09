68ff13d122f9e35fb2754ae1c9bdb53145178feb
naveedAhmed007 <naveed.ahmed007@ymail.com>
2024-05-09 17:06:42 +0500

    email detail

--- Novera/Source/Pages/Crm/Email/EmailDetail/EmailDetailPage.xaml.cs
+++ Novera/Source/Pages/Crm/Email/EmailDetail/EmailDetailPage.xaml.cs
@@ -7,8 +7,10 @@ using Novera.Source.Utility;
 namespace Novera.Source.Pages.Crm.Email.EmailDetail;
 
 public partial class EmailDetailPage : ContentPage
+
 {
     ApiService<EmailDetailResponse> apiService;
+#pragma warning disable CS0612
     public EmailDetailPage()
     {
         InitializeComponent();
@@ -41,8 +43,8 @@ public partial class EmailDetailPage : ContentPage
     public async void getData() {
         try
         {
-            string oauthToken = await SecureStorage.Default.GetAsync("oauth_token");
-            string email_id = await SecureStorage.Default.GetAsync("email_id");
+            string oauthToken = (await SecureStorage.Default.GetAsync("oauth_token")) ?? "";
+            string email_id = (await SecureStorage.Default.GetAsync("email_id")) ?? "";
             string url = $"{ApiUrls.EmailDetail}{email_id}";
 
             var response = await apiService.getAsync(url,  this,oauthToken,HttpMethod.Get);
@@ -50,6 +52,18 @@ public partial class EmailDetailPage : ContentPage
             if (response is EmailDetailResponse successResponse)
             {
                 this.emailDetail.Text = $"Subject: {successResponse.data.emailDetails.subject}\nbody: {successResponse.data.emailDetails.body_Text}";
+                this.from.Text = successResponse.data.emailDetails.sender;
+                first_Char.Text=successResponse.data.emailDetails.sender.Substring(0, 1)[0].ToString();
+                if (successResponse.data.emailDetails.important == "Y")
+                {
+                    status_image.Source = "favourite";
+                }
+                else
+                {
+                    status_image.Source = "email_star";
+
+                }
+
                 if (successResponse?.data?.documents != null) {
                     foreach (var obj in successResponse.data.documents)
                     {
@@ -57,13 +71,12 @@ public partial class EmailDetailPage : ContentPage
                         var label = new Label
                         {
                             Text = obj.documentName,
-                            WidthRequest = 100,
-                            HeightRequest = 100,
                             TextColor = Color.FromRgb(255, 255, 255),
                             VerticalOptions = LayoutOptions.Center,
                             VerticalTextAlignment = TextAlignment.Center,
                             HorizontalTextAlignment = TextAlignment.Center,
-                            Margin = new Thickness(10),
+                            Margin = new Thickness(0),
+                          
 
 
                         };
@@ -73,9 +86,14 @@ public partial class EmailDetailPage : ContentPage
                             BackgroundColor = Color.FromHex("#13294B"),
                             CornerRadius = 5,
                             Padding = 5,
-                            Content = label
+                            Content = label,
+                            WidthRequest=100,
+                            HeightRequest=100,
+                            Margin=5,
+
+                            
                         };
-                        imageStackLayout.Children.Add(label);
+                        imageStackLayout.Children.Add(frame);
                     }
                 }
 
@@ -103,9 +121,7 @@ public partial class EmailDetailPage : ContentPage
         }
         finally
         {
-            // Hide loader
-            //loader.IsRunning = false;
-            //loader.IsVisible = false;
+ 
 
         }
     }
